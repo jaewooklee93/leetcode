6ee80716c362c158e386dc07b45073d9b22521fb
Jaewook Lee <11328376+jaewooklee93@users.noreply.github.com>
2024-01-31 11:45:33 +0900

    finish two-pointer

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # leetcode
-#leetcode #python3
+
+## Two-Pointer
+[1. Two Sum](src/1-two-sum.py)
---
+++ src/1-two-sum.py
@@ -0,0 +1,60 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """ Using Hashmap(dict): O(n) """
+        mem = {}
+        for i, num in enumerate(nums):
+            if num in mem:
+                return [mem[num], i] # mem[num] < i
+            mem[target - num] = i
+
+    def twoSum2(self, nums: List[int], target: int) -> List[int]:
+        """ Using Two-Pointer: O(nlogn) """
+        n = len(nums)
+        nums = sorted((num, i) for i, num in enumerate(nums)) # O(nlogn)
+        
+        # search for (x, y), nums[x] + nums[y] == target
+        # under 0 <= l <= x < y <= r < n
+        l, r = 0, n-1
+        while l < r: # O(n)
+            d = nums[l][0] + nums[r][0] - target
+            if d < 0: l += 1
+            elif d > 0 : r -= 1
+            else: return [nums[l][1], nums[r][1]]
+
+
+def judge(fn):
+    tests = [
+        (
+            {
+                "nums": [2, 7, 11, 15],
+                "target": 9,
+            },
+            [0, 1]
+        ),
+        (
+            {
+                "nums": [3, 2, 4],
+                "target": 6,
+            },
+            [1, 2]
+        ),
+        (
+            {
+                "nums": [3, 3],
+                "target": 6,
+            },
+            [0, 1]
+        ),
+    ]
+    print(fn.__name__)
+    for t in tests:
+        print(t[0])
+        print(t[1])
+        print()
+        assert set(fn(**t[0])) == set(t[1])
+
+if __name__ == "__main__":
+    judge(Solution().twoSum)
+    judge(Solution().twoSum2)
